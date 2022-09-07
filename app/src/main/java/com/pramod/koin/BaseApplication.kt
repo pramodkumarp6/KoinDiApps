053dd71487f1dd1cba87fdb5acd8e54f3d7d608f
pramodkumarp6 <pramodkumarp6@gmail.com>
2022-09-07 17:55:45 +0530

    Initial commit

---
+++ app/src/main/java/com/pramod/koin/BaseApplication.kt
@@ -0,0 +1,6 @@
+package com.pramod.koin
+
+import android.app.Application
+
+class BaseApplication : Application() {
+}
---
+++ app/src/main/java/com/pramod/koin/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.pramod.koin.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModelc : ViewModel() {
+}
